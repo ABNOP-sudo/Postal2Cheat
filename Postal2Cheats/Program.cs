e2619de3e587ef8f7b5822fef76017b0ecc0ffb3
ABNOP-sudo <fox333opka@gmail.com>
2026-05-05 19:10:14 +0200

    Added project files

---
+++ Postal2Cheats/Program.cs
@@ -0,0 +1,26 @@
+﻿using Postal2Cheats;
+using Swed32;
+using System.Threading;
+
+Console.WriteLine("Capturing the game process...");
+
+Swed swed = new Swed("Postal2");
+IntPtr moduleBase = swed.GetModuleBase("Postal2.exe");
+
+Console.WriteLine("Loading addresses...");
+
+IntPtr M16A2AmmoAddress = swed.ReadPointer(moduleBase, 0x0002BFA4, 0x14, 0x14, 0x98) + 0x2CC;
+
+Console.WriteLine("Loading renderer...");
+
+Renderer renderer = new Renderer();
+Thread renderThread = new Thread(() => renderer.Start().Wait());
+renderThread.Start();
+
+while (true)
+{
+    if (renderer.M16A2Bool)
+    {
+        swed.WriteInt(M16A2AmmoAddress, 9999);
+    }
+}
---
+++ Postal2Cheats/Renderer.cs
@@ -0,0 +1,49 @@
+﻿using ClickableTransparentOverlay;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImGuiNET;
+using System.Numerics;
+
+namespace Postal2Cheats
+{
+    public class Renderer : Overlay
+    {
+        public bool customColorsBool = false;
+        public bool M16A2Bool = false;
+        private ImGuiStylePtr styleGUI;
+        public Vector3 colorGUI;
+
+        protected override void Render()
+        {
+            void DefaultColors()
+            {
+                Vector4 standartColor = new Vector4(0.19f, 0.58f, 1f, 0.5f);
+                styleGUI.Colors[(int)ImGuiCol.TitleBg] = standartColor;
+                styleGUI.Colors[(int)ImGuiCol.TitleBgActive] = standartColor;
+                styleGUI.Colors[(int)ImGuiCol.TitleBgCollapsed] = standartColor;
+            }
+
+            styleGUI = ImGui.GetStyle();
+            ImGui.Begin("Cheat GUI");
+            ImGui.Checkbox("M16A2 Infinity ammo", ref M16A2Bool);
+            ImGui.Checkbox("Custom GUI Colors", ref customColorsBool);
+            DefaultColors();
+
+            if (customColorsBool)
+            {
+                ImGui.ColorEdit3("GUI Color", ref colorGUI);
+                Vector4 color4 = new Vector4(colorGUI, 1.0f);
+                styleGUI.Colors[(int)ImGuiCol.TitleBg] = color4;
+                styleGUI.Colors[(int)ImGuiCol.TitleBgActive] = color4;
+                styleGUI.Colors[(int)ImGuiCol.TitleBgCollapsed] = color4;
+            } else
+            {
+                DefaultColors();
+            }
+                ImGui.End();
+        }
+    }
+}
